efa50138c2ceace7435aaf9f8dff37aed2c6ef7a
Lakshay Rana <lakshayrana1358@gmail.com>
2025-03-20 15:39:41 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void fibonacciSeries(int n){
+    int a=0, b=1;
+    for (int i=0; i<n; i++){
+        printf("%d", a);
+        next = a+b;
+        a = b;
+        b = next;
+    }
+    printf("\n");
+}
